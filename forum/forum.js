88f084e027ee28ec9e76dc6f3c961fbb8714a014
EliSmolha <104186351+EliSmolha@users.noreply.github.com>
2025-12-30 17:22:13 +0700

    Update forum.js

--- forum/forum.js
+++ forum/forum.js
@@ -1,10 +1,8 @@
-
-
-
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://sxtktdsknqhrclimcjns.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4dGt0ZHNrbnFocmNsaW1jam5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODM3MDMsImV4cCI6MjA4MjY1OTcwM30.WFFpSVn0eDaF3eqa6wwRtC-81almKzatYnO7q3UgsDc";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4dGt0ZHNrbnFocmNsaW1jam5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODM3MDMsImV4cCI6MjA4MjY1OTcwM30.WFFpSVn0eDaF3eqa6wwRtC-81almKzatYnO7q3UgsDc";
 
 export const supabase = createClient(
   SUPABASE_URL,
@@ -38,4 +36,3 @@ export async function currentUser() {
   const { data } = await supabase.auth.getUser();
   return data.user;
 }
-
